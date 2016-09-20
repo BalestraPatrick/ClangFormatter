75ee45f0ac0fbf9c3b9dfd904ea188ff42f344c9
Boris Bügling <boris@icculus.org>
2016-09-20 21:56:42 +0200

    Cleaned up the code, shared schemes

--- ClangFormat/SourceEditorCommand.swift
+++ ClangFormat/SourceEditorCommand.swift
@@ -13,8 +13,8 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     var commandPath: String {
         return Bundle.main.path(forResource: "clang-format", ofType: nil)!
     }
-    
-      func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
+
+    static func run(commandPath: String, arguments: [String], stdin: String) -> String? {
         let errorPipe = Pipe()
         let outputPipe = Pipe()
 
@@ -22,15 +22,13 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         task.standardError = errorPipe
         task.standardOutput = outputPipe
         task.launchPath = commandPath
-        task.arguments = [ "-style=llvm" ]
-
-        print("Using clang-format \(task.launchPath)")
+        task.arguments = arguments
 
         let inputPipe = Pipe()
         task.standardInput = inputPipe
         let stdinHandle = inputPipe.fileHandleForWriting
 
-        if let data = invocation.buffer.completeBuffer.data(using: .utf8) {
+        if let data = stdin.data(using: .utf8) {
             stdinHandle.write(data)
             stdinHandle.closeFile()
         }
@@ -41,15 +39,27 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         errorPipe.fileHandleForReading.readDataToEndOfFile()
 
         let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        if let outputString = String(data: outputData, encoding: .utf8) {
-            let lines = outputString.characters.split(separator: "\n").map { String($0) }
+        return String(data: outputData, encoding: .utf8)
+    }
+
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
+        if let outputString = SourceEditorCommand.run(commandPath: commandPath,
+                                                      arguments: [ "-style=llvm" ],
+                                                      stdin: invocation.buffer.completeBuffer) ,
+                    invocation.buffer.contentUTI == "public.objective-c-source" {
             invocation.buffer.lines.removeAllObjects()
+
+            let lines = outputString.characters.split(separator: "\n").map { String($0) }
             invocation.buffer.lines.addObjects(from: lines)
 
-            //invocation.buffer.lines.removeAllObjects()
-            //invocation.buffer.selections.removeAllObjects()
-            //invocation.buffer.completeBuffer = outputString
             // Crashes Xcode when replacing `completeBuffer`
+            //invocation.buffer.completeBuffer = outputString
+
+            // If there is a no longer valid selection, Xcode crashes
+            invocation.buffer.selections.removeAllObjects()
+            // and it does the same if there aren't any selections, so we set the insertion point
+            invocation.buffer.selections.add(XCSourceTextRange(start: XCSourceTextPosition(line: 0, column: 0),
+                                                               end: XCSourceTextPosition(line: 0, column: 0)))
         }
 
         completionHandler(nil)
